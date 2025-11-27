96029279ea638b892189c09ba1828236be44e04d
sayklang <sayklang.sar@student.mahidol.edu>
2025-11-27 05:47:14 +0000

    edit

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ let score = 0;
 let countdown;
 
 
+window.addEventListener('mousedown', () => {
+    document.body.classList.add('grabbing');
+});
+
+window.addEventListener('mouseup', () => {
+    document.body.classList.remove('grabbing');
+});
+
+
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
-
 
 function randomHole(holes) {
   const idx = Math.floor(Math.random() * holes.length);
@@ -22,7 +30,6 @@ function randomHole(holes) {
   lastHole = hole;
   return hole;
 }
-
 
 function peep() {
   const time = randomTime(400, 1000);
@@ -39,12 +46,12 @@ function startGame() {
   scoreBoard.textContent = 0;
   timeUp = false;
   score = 0;
-  let timeLeft = 30;
+ 
+  let timeLeft = 30; 
   timeLeftDisplay.textContent = `เวลา: ${timeLeft} วินาที`;
   
   peep();
   
-
   countdown = setInterval(() => {
     timeLeft--;
     timeLeftDisplay.textContent = `เวลา: ${timeLeft} วินาที`;
@@ -57,11 +64,31 @@ function startGame() {
 }
 
 
+function showPlusOne(x, y) {
+    const plusOne = document.createElement('div');
+    plusOne.innerText = '+1';
+    plusOne.classList.add('plus-one');
+    
+  
+    plusOne.style.left = x + 'px';
+    plusOne.style.top = y + 'px';
+    
+    document.body.appendChild(plusOne);
+
+   
+    setTimeout(() => {
+        plusOne.remove();
+    }, 800);
+}
+
 function bonk(e) {
   if(!e.isTrusted) return;
   score++;
   this.parentNode.classList.remove('up'); 
   scoreBoard.textContent = score;
+
+
+  showPlusOne(e.pageX, e.pageY);
 }
 
 moles.forEach(mole => mole.addEventListener('click', bonk));
